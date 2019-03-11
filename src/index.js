d8840759a3ef0c81efa67965418eed98f37236a9
Gonzalo <gonzaloiriart@gmail.com>
2019-03-11 18:09:32 -0300

    asd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 function Greetings() {
-    return <div><h1>Hello world!</h1></div>
+    return (<div>
+        <Person />
+        <Message />
+        </div>)
 };
+
+const Person = () => <h1>John Doe</h1>
+const Message = () => <p>Hello, this is the message</p>
 
 ReactDom.render(<Greetings/>, document.getElementById("root"));
