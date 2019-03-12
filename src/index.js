2cfbb9dbaea8aa6eb756a8836088a0b75add374d
Gonzalo <gonzaloiriart@gmail.com>
2019-03-12 18:04:52 -0300

    day 2

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import './index.css';
 
-function Greetings() {
-    return (<div>
-        <Person />
-        <Message />
-        </div>)
+const employees = [{name : 'Gonzalo Iriart', job : 'Developer', age: '25',company : 'a'},
+{name : 'Jorge perez', job : 'Developer', age: '29',company : 'b'},
+{name : 'Natalia Lopez', job : 'Developer', age: '23',company : 'c'}];
+
+function People() {
+    return (<section>
+    <Person person = {employees[0]}><p>more info</p></Person>
+    <Person person = {employees[1]}/>
+    <Person person = {employees[2]}/>
+        </section>)
 };
 
-const Person = () => <h1>John Doe</h1>
-const Message = () => <p>Hello, this is the message</p>
+const Person = props=>{
 
-ReactDom.render(<Greetings/>, document.getElementById("root"));
+    const {name,job,age,company} = props.person;
+    const {children} = props;
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p>{job}</p>
+            <p>{age}</p>
+            <p>{company}</p>
+            <p>{children}</p>
+            <hr />
+        </div>
+        );
+};
+
+
+ReactDom.render(< People/>, document.getElementById("root"));
